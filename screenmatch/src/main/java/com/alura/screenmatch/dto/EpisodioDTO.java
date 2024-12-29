e9cb750c58ce8e06dbc26e1abb0c8f72d2364f44
Santiago Suarez <sansu801@gmail.com>
2024-12-29 16:58:27 -0500

    Second commit

---
+++ screenmatch/src/main/java/com/alura/screenmatch/dto/EpisodioDTO.java
@@ -0,0 +1,7 @@
+package com.alura.screenmatch.dto;
+
+public record EpisodioDTO(Integer temporada,
+    String titulo,
+    Integer numeroEpisodio) {
+
+}
--- screenmatch/src/main/java/com/alura/screenmatch/repository/SerieRepository.java
+++ screenmatch/src/main/java/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,5 +1,6 @@
 package com.alura.screenmatch.repository;
 
+import com.alura.screenmatch.dto.EpisodioDTO;
 import com.alura.screenmatch.model.Categoria;
 import com.alura.screenmatch.model.Episodio;
 import com.alura.screenmatch.model.Serie;
@@ -19,10 +20,16 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     @Query("SELECT s FROM Serie s WHERE s.totalDeTemporadas <= :totalDeTemporadas AND s.evaluacion >= :evaluacion")
     List<Serie> seriesPorTemporadaYEvaluacion(int totalDeTemporadas, Double evaluacion);
 
+    @Query("SELECT s FROM Serie s " + "JOIN s.episodios e " + "GROUP BY s " + "ORDER BY MAX(e.fechaDeLanzamiento) DESC LIMIT 5")
+    List<Serie> lanzamientosMasRecientes();
+
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:nombreEpisodio%")
     List<Episodio> episodiosPorNombre(String nombreEpisodio);
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.evaluacion DESC LIMIT 5")
     List<Episodio> top5Episodios(Serie serie);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numeroTemporada")
+    List<Episodio> obtenerTemporadasPorNumero(Long id, Long numeroTemporada);
 }
